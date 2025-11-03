3640d1f5626add48a981a7a7034b7437fc8adf99
Darell Rangga <darrelrangga@gmail.com>
2025-11-03 13:27:31 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BerandaController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])
+    ->name('backend.beranda');
+
+Route::get('backend/login', [LoginController::class, 'loginBackend'])
+    ->name('backend.login');
+Route::post('backend/login', [LoginController::class, 'authenticateBackend'])
+    ->name('backend.login');
+Route::post('backend/logout', [LoginController::class, 'logoutBackend'])
+    ->name('backend.logout');
+
+// Route::resource('backend/user', UserController::class)->middleware('auth'); 
+Route::resource('backend/user', UserController::class, ['as' => 'backend'])
+    ->middleware('auth');
